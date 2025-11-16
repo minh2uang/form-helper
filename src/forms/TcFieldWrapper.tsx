15712bfbb3e977f2d2887460155febd9a66e734f
quang <mmqqq19999@gmail.com>
2025-11-16 16:40:42 +1100

    enhanced

--- src/forms/TcFieldWrapper.tsx
+++ src/forms/TcFieldWrapper.tsx
@@ -1,23 +1,26 @@
-import { Typography } from "@mui/material";
-import React from "react";
+import { Box, Tooltip, Typography } from '@mui/material'
+import { JSX } from 'react'
 
 type Props = {
-  error?: string;
-  isDirty?: boolean;
-  children?: JSX.Element;
-};
+  error?: string
+  info?: string
+  isDirty?: boolean
+  children?: JSX.Element
+}
 
-const TcFieldWrapper = ({ children, error, isDirty }: Props) => {
+const TcFieldWrapper = ({ children, error, info, isDirty }: Props) => {
   return (
-    <>
-      {children}
-      {error && isDirty && (
-        <Typography color="error" variant="body2">
-          {error}
-        </Typography>
-      )}
-    </>
-  );
-};
-
-export default TcFieldWrapper;
+    <Tooltip arrow title={info} placement="top">
+      <Box>
+        {children}
+        {error && isDirty && (
+          <Typography color="error" variant="body2">
+            {error}
+          </Typography>
+        )}
+      </Box>
+    </Tooltip>
+  )
+}
+export type TcFieldWrapper<T> = T & Props
+export default TcFieldWrapper
--- src/forms/TcTextField.tsx
+++ src/forms/TcTextField.tsx
@@ -1,17 +1,27 @@
-import React, { useCallback } from 'react'
-import { TextField, Typography } from '@mui/material'
-import FieldWrapper from './TcFieldWrapper'
-import TcFieldProps from '../types/Form/TcFieldProps'
+import { TextField } from '@mui/material'
+import FieldWrapper, { TcFieldWrapper } from './TcFieldWrapper'
+import TcFieldProps from './TcFieldProps'
 
-interface Props extends TcFieldProps<string> {
+export interface TcTextFieldProps extends TcFieldWrapper<TcFieldProps<string>> {
   type?: string
+  rows?: number
 }
 
-const TcTextField = ({ onChange, error, isDirty, value, ...props }: Props) => {
+const TcTextField = ({
+  onChange,
+  error,
+  isDirty,
+  value,
+  info,
+  ...props
+}: TcTextFieldProps) => {
   return (
-    <FieldWrapper isDirty={isDirty} error={error}>
+    <FieldWrapper isDirty={isDirty} error={error} info={info}>
       <TextField
+        fullWidth
+        size="small"
         {...props}
+        InputLabelProps={{ shrink: true }}
         value={value || ''}
         onChange={({ target }) => onChange(target.value)}
       />
